fb436d018a0dc136cbfb4ab28662b892337595b6
Oscar <oscareinarsson@hotmail.com>
2024-03-07 15:21:11 +0100

    2

--- Assignment-3/TSP.py
+++ Assignment-3/TSP.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random as rand
+import time
 def read_items():
     cities = {}
     with open('C:/Users/oscar/OneDrive\Dokument/GitHub/AI-Labs/Assignment-3/berlin52.tsp', 'r') as file:
@@ -24,11 +25,10 @@ def calculate_distance(city1, city2):
 def generate_genome():
     genome = []
     genome = rand.sample(range(2, 53), 51)
-    genome.append(1)
+    genome.insert(0, 1)
     return genome
 #Initiate first population
-def init_population():
-    pop_size = 520
+def init_population(pop_size):
     population = []
     for _ in range(0, pop_size):
         population.append(generate_genome())
@@ -37,7 +37,8 @@ def init_population():
 def eval_fitness(genome, cities):
     fitness_value = 0 
     for i in range(len(genome) - 1):
-        fitness_value -= calculate_distance(cities[genome[i]], cities[genome[i+1]])
+        fitness_value += calculate_distance(cities[genome[i]], cities[genome[i+1]])
+    fitness_value += calculate_distance(cities[genome[i + 1]], cities[1])
                
     return fitness_value
 #Might want to add so that the fittest gnome will survive to the next gen
@@ -45,62 +46,94 @@ def selection(population, cities):
     k = 2
     parents = []
     for _ in range((len(population) // k)):
-        tournament = rand.sample(population, 2)
+        tournament = rand.sample(population, k)
         fitness_scores = [eval_fitness(genome, cities) for genome in tournament]
-        winner_index = fitness_scores.index(min(fitness_scores))
-        loser_index = fitness_scores.index(max(fitness_scores))
-        parents.append(tournament[winner_index])
-        population.remove(tournament[winner_index])
-        population.remove(tournament[loser_index])
+        for _ in range(k//2):
+            winner_index = fitness_scores.index(min(fitness_scores))
+            loser_index = fitness_scores.index(max(fitness_scores))
+            parents.append(tournament[winner_index])
+            population.remove(tournament[winner_index])
+            population.remove(tournament[loser_index])
     return parents
 
-def repair(genome, cities): 
+def repair(genome): 
     repaired_genome = []
     visited_cities = set()
     for city in genome:
         if city not in visited_cities:
             repaired_genome.append(city)
             visited_cities.add(city)
-    for city in range(1, len(cities) + 1):
-        if city not in visited_cities:
-            repaired_genome.append(city)
     return repaired_genome
-#Craes here prob
-def crossover(parent1, parent2, cities):
-    crossover_point1 = rand.randint(0, len(parent1)-1)
-    crossover_point2 = rand.randint(0, len(parent1)-1)
-    while crossover_point1 == crossover_point2:
-        crossover_point2 = rand.randint(0, len(parent1)-1)
-    if crossover_point1 > crossover_point2:
-        crossover_point1, crossover_point2 = crossover_point2, crossover_point1
+
+
+def crossover(parent1, parent2):
+    #crossover_point1 = rand.randint(1, (len(parent1)-1))
+    #crossover_point2 = rand.randint(1, (len(parent1)-1))
+    crossover_point1, crossover_point2 = rand.sample(range(1, (len(parent1)-1)), 2)
+    while (crossover_point2 - crossover_point1) > 25 or (crossover_point2 - crossover_point1) < 5:
+        crossover_point1, crossover_point2 = rand.sample(range(1, (len(parent1)-1)), 2)
     offspring = parent1[:]
     offspring[crossover_point1 : crossover_point2 + 1] = parent2[crossover_point1 : crossover_point2 + 1]
     missing_cities = [city for city in parent1 if city not in offspring[crossover_point1:crossover_point2 + 1]]
     offspring[crossover_point2 + 1:] = missing_cities
-    offspring = repair(offspring, cities)
+    offspring = repair(offspring)
+    if rand.randint(1, 8) == 1:           
+        mutate_swap(offspring)
+        
+   
     return offspring
 
 def populate_new_gen(population, cities):
     new_gen = []
-    for _ in range((len(population))):
+    fitness_scores = [eval_fitness(genome, cities) for genome in population]
+    for _ in range(2):       
+        winner_index = fitness_scores.index(min(fitness_scores))
+        new_gen.append(population[winner_index])
+        fitness_scores.remove(fitness_scores[winner_index])
+    
+    for _ in range((len(population))-1):
         parents = rand.sample(population, 2)
         parent1 = parents[0]
         parent2 = parents[1]
-        offspring = crossover(parent1, parent2, cities)
-        new_gen.append(offspring)
-    population.extend(new_gen)
-    return population
+        offspring1 = crossover(parent1, parent2)
+        offspring2 = crossover(parent2, parent1)
+        new_gen.append(offspring1)
+        new_gen.append(offspring2)
+    
+    
+    return new_gen
+
+def mutate_inv(genome):
+    mutated_genome = genome[::-1]
+    return mutated_genome
+def mutate_swap(genome):
+    index1, index2 = rand.sample(range(2, len(genome)), 2)
+    genome[index1], genome[index2] = genome[index2], genome[index1]
+    return genome
     
 
+
+
+    
+    
+generations = 1000
+pop_size = 104
 cities = read_items()
 genome = [] #52 size
 population = [] #520-1040 size
 fitness_scores = []
-population = init_population()
-next_gen = selection(population, cities)
-print(len(next_gen))
-new_gen = populate_new_gen(next_gen, cities)
-print(len(new_gen))
+population = init_population(pop_size)
+start = time.time()
+for _ in range(0, generations):
+    population = selection(population, cities)
+    population = populate_new_gen(population, cities)
+    print("New generation")
+fitness_scores = [eval_fitness(genome, cities) for genome in population]
+winner_index = fitness_scores.index(min(fitness_scores))
+end = time.time()
+print("Time:", end-start)
+print(population[winner_index])
+print(eval_fitness(population[winner_index], cities))
 
 
 
