e169b4cfd514a4193bc6173cb3e00a8bf13313ac
Oscar <oscareinarsson@hotmail.com>
2024-03-15 17:19:12 +0100

    ant done

--- Assignment-3/ACO.py
+++ Assignment-3/ACO.py
@@ -1,38 +1,49 @@
 import numpy as np
+import random as rand
 
-ALPHA = 0.9
-BETA = 1.5 
+
 class Ant:
     def __init__(self, num_cities):
         self.path = []
-        self.visited = [False]*num_cities
+        self.unvisited = [i for i in range(0, num_cities)]
      
-    def select_next_city(self, phermones, distances):
-        next_city = 0
-        if not self.path:
-            current_city = 0
-        else:    
-            current_city = self.path.pop()
-        total_probability = sum(phermones[current_city][city] * (1.0 / distances[current_city][city]) for city in cities if city != current_city)
+    def select_next_city(self, phermones, distances, current_city, unvisited_cities):
+        selected_city = None
+        total_probability = sum(((phermones[current_city][city]) ** alpha) * (((1.0 / distances[current_city][city]) ** beta) + 0.01) for city in unvisited_cities)
         probabilities = {}
-        for city in cities:
-            if city != current_city:
-                phermone_level = phermones[current_city][city]
-                distance = distances[current_city][city]
-                probability = (phermone_level * (1.0 / distance)) / total_probability
-                probabilities[city] = probability
-        for city, probability in probabilities.items():
-            print(city, probability)
-                
-            
-        print("AAAAAAAAAAAAAAAAAA")
-        return next_city
+        for city in unvisited_cities:    
+            phermone_level = phermones[current_city][city]
+            distance = distances[current_city][city]
+            probability = (((phermone_level ** alpha) * ((1.0 / distance) ** beta)) / total_probability)
+            probabilities[city] = probability
+        selected_city = roulette_selection(probabilities)
+        return selected_city
+    
+def roulette_selection(probabilities):
+    selected_city = None
+    total_probability = sum(probabilities.values())
+    probability = rand.uniform(0, total_probability)
+    cumu_probability = 0.0
+    for p in probabilities:
+        cumu_probability += probabilities[p]
+        if cumu_probability >= probability:
+            selected_city = p
+            return selected_city
+    return None
+
+
         
 def calc_distance(city1, city2):
     (x1, y1) = city1
     (x2, y2) = city2
     return np.sqrt((np.power(x2 - x1, 2)) + (np.power(y2 - y1, 2)))
 
+def calc_total_distance(path):
+    total_distance = 0
+    for i in range(len(path)-1):
+        total_distance += calc_distance(cities[path[i]], cities[path[i+1]])
+    total_distance += calc_distance(cities[path[i+1]], cities[0])
+    return total_distance
 def calc_normalized_distance(city1, city2, max_distance):
     (x1, y1) = city1
     (x2, y2) = city2
@@ -68,35 +79,64 @@ def init_tables(cities, phermone_value, max_distance):
     for i in range(len(cities)):
         for j in range(len(cities)):
             if i == j:
-                phermone_table[i][j] = -1
+                phermone_table[i][j] = None
             else:
                 phermone_table[i][j] = phermone_value
             distance_table[i][j] = calc_normalized_distance(cities[i], cities[j], max_distance)
             distance_table[i][j] = calc_normalized_distance(cities[j], cities[i], max_distance)
     return phermone_table, distance_table
 
-
-def run_aco(phermone_table, distances, ant_amount, iterations, evaporation):
-    best_path = []
-    best_distance = 0
-    for iteration in range(iterations):
-        ants = [Ant(len(cities)) for _ in range(ant_amount)]
-        for ant in ants:
-            current_path = []
-            current_distance = 0
-            while not all(ant.visited):
-                next_city = ant.select_next_city(phermone_table, distances)
-            
+def update_pheromone_levels(p_levels, evap_rate, ants):
+    for i in range(len(cities)):
+        for j in range(len(cities)):
+            if p_levels[i][j] == None:
+                continue
+            else:
+                p_levels[i][j] -= evap_rate
+    for ant in ants:
+        for i in range(len(ant.path)-1):
+            p_levels[ant.path[i]][ant.path[i+1]] += deposition_value
+        p_levels[ant.path[i+1]][ant.path[0]] += deposition_value
         
-        if current_distance < best_path:
-            best_path = current_path
-    return best_path
+        
+def run_aco(phermone_table, distances, ant_amount, iterations, evaporation):
+    best_path = None
+    best_distance = float('inf')
+    for _ in range(iterations):
+        ants = [Ant(len(cities)-1) for _ in range(ant_amount)]
+        print("")
+        for ant in ants:
+            current_city = 0
+            while len(ant.unvisited) > 0:
+                ant.path.append(current_city)
+                ant.unvisited.remove(current_city)
+                if not ant.unvisited:
+                    print("")
+                next_city = ant.select_next_city(phermone_table, distances, current_city, ant.unvisited)
+                current_city = next_city
+                print("Ant searching...")
+        print("Ant done...")
+        for ant in ants:
+            path = ant.path
+            total_distance = calc_total_distance(path)
+            if best_distance > total_distance:
+                best_path = path
+                best_distance = total_distance         
+        update_pheromone_levels(phermone_table, evaporation, ants)
+        print("Updating pheromones...")
+    return best_distance, best_path
+
 
 cities = read_items()
-phermone_value = 10
-evaporation_rate = 0.01
-ant_amount = len(cities)
-iterations = 10
+alpha = 2
+beta = 3
+phermone_value = 1.0
+evaporation_rate = 0.8
+deposition_value = 2
+ant_amount = 104
+iterations = 6
 phermone_table, distance_table = init_tables(cities, phermone_value, max_distance=0.0)
-run_aco(phermone_table, distance_table, ant_amount, iterations, evaporation_rate)
+distance, path = run_aco(phermone_table, distance_table, ant_amount, iterations, evaporation_rate)
+print("Distance:", distance)
+print("Path:", path)
 
